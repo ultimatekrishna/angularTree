f9b7c553d7d4b463c9292470ea6153c45eb91fd5
Krishna Singh <krishna@Krishnas-MacBook-Air.local>
2017-11-28 01:43:55 +0530

    setting up bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { AccordionBasicComponent } from './accordion-basic/accordion-basic.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionBasicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
