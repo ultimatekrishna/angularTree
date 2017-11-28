833ba29b4b70a6a863ec0077b24dc6aea70e0bac
Krishna Singh <mail.krishnasingh@gmail.com>
2017-11-28 16:32:05 +0530

    fixing container

---
+++ src/app/container-basic/container-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-basic',
+  templateUrl: './container-basic.component.html',
+  styleUrls: ['./container-basic.component.css']
+})
+export class ContainerBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
